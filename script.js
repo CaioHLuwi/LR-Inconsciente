233b9325f180adac5898b1181093f7c50d6e5a7b
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-21 12:10:10 -0300

    feat: muda os estilos do cabeçalho ao scrollar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttonMenu = document.querySelectorAll('.categoriaMenu')
+const header = document.querySelector('#cabecalho')
 
 for(let i = 0; i < buttonMenu.length; i++){
     buttonMenu[i].addEventListener('click', () => {
@@ -12,3 +13,9 @@ function removeActive() {
         target.classList.remove('active')
     })
 }
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("active", window.scrollY > 50);
+})
+
+
